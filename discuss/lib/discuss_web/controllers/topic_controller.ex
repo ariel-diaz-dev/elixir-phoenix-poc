2a9c2e65c1c3c75925447f6b8d0c954f5ed2edbe
Ariel Diaz <arieldiaz92@gmail.com>
2021-11-20 05:17:33 -0500

    adding new route

---
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,13 @@
+defmodule DiscussWeb.TopicController do
+  use DiscussWeb, :controller
+
+  def new(conn, params) do
+    IO.puts "++++++"
+    IO.inspect conn
+    IO.puts "++++++"
+    IO.inspect params
+    IO.puts "++++++"
+
+    #render(conn, "index.html")
+  end
+end
