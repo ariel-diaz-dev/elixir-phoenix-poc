234b2f7fa789854eaebad67e8e76c0d90333267a
Ariel Diaz <arieldiaz92@gmail.com>
2021-11-22 14:08:48 -0500

    sending event

--- discuss/lib/discuss_web/channels/comments_channel.ex
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -8,34 +8,18 @@ defmodule DiscussWeb.CommentsChannel do
   def join("comments:" <> topic_id, payload, socket) do
     topic_id = String.to_integer(topic_id)
     topic = Repo.get(Topic, topic_id)
-
-    IO.puts "+++++++++++++++++++"
-    IO.inspect topic
-    IO.puts "+++++++++++++++++++"
-
     if authorized?(payload) do
-      {:ok, %{}, socket}
+      {:ok, %{}, assign(socket, :topic, topic), socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+  def handle_in(name, %{"content" => content}, socket) do
+    IO.puts(content)
+    {:reply, :ok, socket}
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (comments:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
-    {:noreply, socket}
-  end
-
-  # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
   end
